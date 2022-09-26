36e503d42ec346947f78d8c00f5ed19f7c3d1e75
lukapavlovic <lpavlovic11521rn@raf.rs>
2022-09-26 19:11:03 +0200

    Initial commit

---
+++ src/main/java/com/raf/example/HotelApiGetawayService/HotelApiGetawayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.example.HotelApiGetawayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApiGetawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApiGetawayServiceApplication.class, args);
+	}
+
+}
